56944a059a529c3da9da1ebbd62dda4da75e97db
OSmall <oliver.small232@gmail.com>
2024-09-14 17:11:18 +1000

    split terraform into core and common

--- iac/core/main.tf
+++ iac/core/main.tf
@@ -29,34 +29,34 @@ resource "aws_dynamodb_table" "authjs" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "pk"
   range_key    = "sk"
- 
+
   attribute {
     name = "pk"
     type = "S"
   }
- 
+
   attribute {
     name = "sk"
     type = "S"
   }
- 
+
   attribute {
     name = "GSI1PK"
     type = "S"
   }
- 
+
   attribute {
     name = "GSI1SK"
     type = "S"
   }
- 
+
   global_secondary_index {
     name            = "GSI1"
     hash_key        = "GSI1PK"
     range_key       = "GSI1SK"
     projection_type = "ALL"
   }
- 
+
   ttl {
     attribute_name = "expires"
     enabled        = true
