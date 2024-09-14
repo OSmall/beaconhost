41ab5fbf6c4c96e951a64e3d2f0b566a4d3439aa
OSmall <oliver.small232@gmail.com>
2024-09-14 21:03:36 +1000

    change env variable structure

--- iac/core/.terraform.lock.hcl
+++ iac/core/.terraform.lock.hcl
@@ -35,3 +35,44 @@ provider "registry.opentofu.org/opentofu/aws" {
     "zh:d39f5e3e101bac6da0ba782c27e1eb47457fbbb4640be1c055625aab20eae354",
   ]
 }
+
+provider "registry.opentofu.org/opentofu/random" {
+  version     = "3.6.2"
+  constraints = "3.6.2"
+  hashes = [
+    "h1:V5b9JmAN3KmYnWrptzPHfVn1ZOSALkMoMM2FJBR1ak8=",
+    "zh:1f27612f7099441526d8af59f5b4bdcc35f46915df5d243043d7337ea5a3e38a",
+    "zh:2a58e66502825db8b4b96116c04bd0323bca1cf1f5752bdd8f9c26feb84d3b1e",
+    "zh:4f0a4fa479e29de0c3c90146fd58799c097f7a55401cb00560dd4e9b1e6fad9d",
+    "zh:9c93c0fe6ef685513734527e0c8078636b2cc07591427502a7260f4744b1af1d",
+    "zh:a466ff5219beb77fb3b18a3d7e7fe30e7edd4d95c8e5c87f4f4e3fe3eeb8c2d7",
+    "zh:ab33e6176d0c757ddb31e40e01a941e6918ad10f7a786c8e8e4f35e5cff81c96",
+    "zh:b6eabf377a1c12cb3f9ddd97aacdd5b49c1646dc959074124f81d40fcd216d7e",
+    "zh:ccec5d03d0d1c0f354be299cdd6a417b2700f1a6781df36bcce77246b2f57e50",
+    "zh:d2a7945eeb691fdd2b1474da76ddc2d1655e2aedbb14b57f06d4f5123d47adf9",
+    "zh:ed62351f4ad9d1469c6798b77dee5f63b18b29c473620a0046ba3d4f111b621d",
+  ]
+}
+
+provider "registry.opentofu.org/vercel/vercel" {
+  version     = "1.13.0"
+  constraints = "~> 1.0"
+  hashes = [
+    "h1:p59w9pw7SF4w52rQ3oCMWnhEAnH0wGWf8z4tCCPf1fI=",
+    "zh:16c126a0e617017ed890b7ad82f82aed656638ffbab1764c7f273ae7c55bc9cb",
+    "zh:2c69e9272dc89a41a3ede1759bbd7926739f9c2748fc1206f0dbc65f0e0a4d7c",
+    "zh:2d71a002a3b504a98e57894079dcfd84f5ed4668cf1883513d924c2e3a9ee828",
+    "zh:32982df551c2cfe1e9fa49289eaa0dbb7eb93c43fbd614e834095041cb4c60fd",
+    "zh:4d0adc24217214c3d269a815ff6fa7e26dd202095432ed2988755d209ce4e67d",
+    "zh:593a0c7caa7a858a6c17491b9b4fe5f26c2560fa8e2d518d75f5186a04e94d34",
+    "zh:7c38143da6ed8d47eb52b84872b23d8e3907353fded5cba75b6588089a2c2f58",
+    "zh:9a613c6cb82ec73fa0fc0a03f450742f7e103edcab217290c3a34d5d3c497b0b",
+    "zh:a518dea6324f058bdf15ef8cd31b4875c864eed7478e7015bafc37c14235bd34",
+    "zh:a5ba2f8e0504991f280a1a095460675dd6c7435654c71186158aea950be668cf",
+    "zh:d0c19c28a9019af90f12a6b072f3c5dc336332c044cf7d7f1312619ae8c52442",
+    "zh:ee8e0b387e8bdea6b1e1948f81f98fcc705fe313feb5dc2b54bf7840d38096bb",
+    "zh:f03717a6f52fad1bf0b1cbebc32a0569be023909e60298f44efe75034dc5b0c4",
+    "zh:f26e0763dbe6a6b2195c94b44696f2110f7f55433dc142839be16b9697fa5597",
+    "zh:fa93e28673a688a7d72e367f93acf7c79db6b1b4cbd1a2804a18d5d7dba5c521",
+  ]
+}
--- iac/core/main.tf
+++ iac/core/main.tf
@@ -1,8 +1,26 @@
 variable "aws_region" {
+  type    = string
   default = "ap-southeast-2"
 }
 
 variable "environment" {
+  type = string
+}
+
+variable "vercel_aws_access_key_id" {
+  type = string
+}
+
+variable "vercel_aws_secret_access_key" {
+  type = string
+}
+
+variable "vercel_github_id" {
+  type = string
+}
+
+variable "vercel_github_secret" {
+  type = string
 }
 
 terraform {
@@ -10,6 +28,14 @@ terraform {
     aws = {
       source  = "opentofu/aws"
       version = "~> 5.0"
+    }
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "opentofu/random"
+      version = "3.6.2"
     }
   }
 }
@@ -61,4 +87,55 @@ resource "aws_dynamodb_table" "authjs" {
     attribute_name = "expires"
     enabled        = true
   }
+}
+
+data "vercel_project" "beaconhost_webapp" {
+  name = "beaconhost-webapp"
+}
+
+resource "vercel_project_environment_variable" "stage" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "STAGE"
+  value      = "dev"
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
+}
+
+resource "vercel_project_environment_variable" "aws_access_key_id" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "AWS_ACCESS_KEY_ID"
+  value      = var.vercel_aws_access_key_id
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
+}
+
+resource "vercel_project_environment_variable" "aws_secret_access_key" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "AWS_SECRET_ACCESS_KEY"
+  value      = var.vercel_aws_secret_access_key
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
+}
+
+resource "vercel_project_environment_variable" "github_id" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "GITHUB_ID"
+  value      = var.vercel_github_id
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
+}
+
+resource "vercel_project_environment_variable" "github_secret" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "GITHUB_SECRET"
+  value      = var.vercel_github_secret
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
+}
+
+resource "random_string" "auth_secret" {
+  length  = 64
+  special = true
+}
+
+resource "vercel_project_environment_variable" "auth_secret" {
+  project_id = data.vercel_project.beaconhost_webapp.id
+  key        = "AUTH_SECRET"
+  value      = random_string.auth_secret.result
+  target     = var.environment == "prod" ? ["production"] : ["preview"]
 }
